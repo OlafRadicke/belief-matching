5d65a7f9dc4fb6f1af767b11b08b6addd0917de3
Olaf Radicke <briefkasten@olaf-radicke.de>
2012-05-27 00:18:57 +0200

    ein schritt weiter mit checkliste.

--- belief-matching.py
+++ belief-matching.py
@@ -2,9 +2,7 @@
 # -*- coding: latin-1 -
 
 import sys
-
-sys.path.append("../lib")
-
+import sqlite3
 import web
 from web import form
 
@@ -16,11 +14,16 @@ app = web.application(urls, globals())#, web.reloader)
 render = web.template.render('templates/')
 
 test_form = form.Form( 
-    form.Textbox('source',
-                 form.notnull,
-                 #form.regexp('^-?\d+$', 'Not a number.'),
-                 #form.Validator('Not greater 10.', lambda x: int(x)>10),
-                 description='Enter a source:'),
+    form.Checkbox("yes_or_no", 
+                  description="trifft zu", 
+                  class_="standard", 
+                  value="", 
+                  checked=False),                      
+    #form.Textbox('source',
+                 #form.notnull,
+                 ##form.regexp('^-?\d+$', 'Not a number.'),
+                 ##form.Validator('Not greater 10.', lambda x: int(x)>10),
+                 #description='Enter a source:'),
     form.Button('go')
     )
     
@@ -86,13 +89,15 @@ class test:
         htmlcode = htemp.top("test")
         htmlcode += '        <h2>Test</h2>'
         htmlcode += '            <form method="POST" name="test">'
-        htmlcode += _form.render()
+        #htmlcode += _form.render()
+        htmlcode += '<input type="checkbox" name="yes_or_no" value="yes_or_no" /> '
+        htmlcode += form.Button('go').render()
         htmlcode += '            </form>'
         htmlcode += htemp.bottom
         return htmlcode       
         
     def POST(self): 
-        _form = compare_form() 
+        _form = test_form() 
         if not _form.validates(): 
             htmlcode = htemp.top("test")
             htmlcode += '        <h2>Test</h2>'
@@ -102,11 +107,19 @@ class test:
             htmlcode += htemp.bottom
             return htmlcode        
         else:
-            hiltirepo = _form['source'].value
+            #hiltirepo = _form['source'].value
+            yes_or_no = _form['yes_or_no'].value
+            #searchTerms = web.input()
+            widgetlist = web.input(groups = []) 
+            #print widgetlist["yes_or_no"]
             
             htmlcode = htemp.top("test")
             htmlcode += '        <h2>Ergebnis</h2>'
-            htmlcode +=  "compare( source=" + hiltirepo  + " )"
+            #htmlcode +=  "yes_or_no=" + yes_or_no  
+            if "yes_or_no" in widgetlist: 
+                htmlcode += 'yes_or_no=YES'
+            else:
+                htmlcode += 'yes_or_no=NO'
             htmlcode += htemp.bottom
             return htmlcode        
          
---
+++ templates/create-sqlite-de.sql
@@ -0,0 +1,55 @@
+BEGIN;
+
+-- ########## create tables ################
+
+CREATE TABLE questions (
+    question_id INTEGER PRIMARY KEY NOT NULL,
+    question    TEXT    NOT NULL,
+    kat         TEXT    NOT NULL
+);
+
+CREATE TABLE answer (
+    answer_id      INTEGER PRIMARY KEY AUTOINCREMENT,
+    question_id    INTEGER NOT NULL,
+-- value is "y" or "n"
+    yes_or_no      TEXT    NOT NULL,
+-- value is "y" or "n"
+    important      TEXT    NOT NULL,
+-- value is "y" or "n"
+    disregard      TEXT    NOT NULL,
+    FOREIGN KEY ( question_id ) REFERENCES question ( question_id )
+);
+
+-- ######### insert questions #############
+
+-- INSERT INTO questions kat, question VALUES (
+-- "",
+-- "");
+
+-- ======= kat "Gott" =============
+
+INSERT INTO questions kat, question VALUES (
+"Gott",
+"Ich glaube an die dreifaltigkeit");
+INSERT INTO questions kat, question VALUES (
+"Gott",
+"Jesus ist nicht Gott sondern nur ein (besonderer) Mensch");
+INSERT INTO questions kat, question VALUES (
+"Gott",
+"Gott offenbart sich direkt, auch ohne die Bibel und Klerus");
+
+-- ======== Kat "Taufe" ============
+INSERT INTO questions kat, question VALUES (
+"Taufe",
+"Die Taufe ist Voraussetzung für die Erlösung/Errettung");
+INSERT INTO questions kat, question VALUES (
+"Taufe",
+"Die Taufe wird äußerlich durch Wasser vollzogen");
+INSERT INTO questions kat, question VALUES (
+"Taufe",
+"Ich lehne Kindertaufe ab");
+
+-- ########### insert answer ################
+
+
+COMMIT;
