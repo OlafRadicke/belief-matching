eabbdf2656cb39392176f2a7d7b17e7d2d4652e5
Olaf Radicke <briefkasten@olaf-radicke.de>
2012-05-28 12:36:52 +0200

    neuer Reiter und neues css.

--- belief-matching.py
+++ belief-matching.py
@@ -9,7 +9,8 @@ from web import form
 
 urls = (
   '/', 'index',
-  '/test', 'test')
+  '/test', 'test',
+  '/datenbasis','datenbasis')
 
 app = web.application(urls, globals())#, web.reloader)
 render = web.template.render('templates/')
@@ -45,13 +46,17 @@ class HtmlTemplate:
         top +=    '        <div class="line" ></div>'
         top +=    '           <ul id="portal-globalnav">'
         if ( aktivtab == "home"):
-            top +=    '            <li id="activ"><a href="/">Start</a></li>'
+            top +=    '            <li id="tableft_activ"><a href="/">Start</a></li>'
         else:
-            top +=    '            <li><a href="/">home</a></li>'
+            top +=    '            <li id="tableft" ><a href="/">home</a></li>'
         if ( aktivtab == "test"):
-            top +=    '            <li id="activ"><a href="test">Test</a></li>'
+            top +=    '            <li id="tabmiddle_activ"><a href="test">Test</a></li>'
         else:
-            top +=    '            <li><a href="test">test</a></li>'
+            top +=    '            <li id="tabmiddle"><a href="test">test</a></li>'
+        if ( aktivtab == "datenbasis"):
+            top +=    '            <li id="tabright_activ"><a href="datenbasis">datenbasis</a></li>'
+        else:
+            top +=    '            <li id="tabright"><a href="datenbasis">datenbasis</a></li>'
         top +=    '           </ul>'
         top +=    '        </div>'
         top +=    '    <div class="appbox">'  
@@ -77,7 +82,7 @@ class index:
         htmlcode = htemp.top("home")
         htmlcode += '        <h2>Willkommen auf dem BELIEF MATCHING</h2>'
         htmlcode += 'Ein Tool um herauszufinden mit welchen Glaubensgemeinschaften '
-        htmlcode += 'deine eigene Überzeugeng am meisten übereinstimmt.'
+        htmlcode += u'deine eigene Überzeugeng am meisten übereinstimmt.'
         htmlcode += htemp.bottom
         return htmlcode 
 
@@ -105,7 +110,7 @@ class test:
             htmlcode += '          <tr>'
             htmlcode += '            <td>' + row[1] + '</td>'
             htmlcode += u'            <td>' + unicode(row[2]) + '</td>'
-            htmlcode += '            <td><input type="checkbox" name="' + str(row[0]) \
+            htmlcode += '            <td>Trift zu:<input type="checkbox" name="' + str(row[0]) \
                 + '" value="' + str(row[0]) + '" /></td>'
             htmlcode += '            <td><select name="' + str(row[0]) + '-wichtung" size="1">'
             htmlcode += '                  <option value="1">mittel</option>'
@@ -151,8 +156,75 @@ class test:
         htmlcode += htemp.bottom
         return htmlcode        
          
+class basisdaten:      
+    def GET(self):
+        _form = test_form()
+        #return render.index(my_form)
+        conn = sqlite3.connect('belief-matching.sqlite')
+        cur = conn.cursor()
+        cur.execute("SELECT question_id, kat, question FROM questions order by kat;")
         
+        htmlcode = htemp.top("datenbasis") 
+        htmlcode += '        <h2>Test</h2>'
+        htmlcode += '          <form method="POST" name="test">'      
+        htmlcode += '            <table>'
+        htmlcode += '              <tr>'
+        htmlcode += '                <th>Kategorie</th>'
+        htmlcode += u'                <th>&Uuml;berzeugnung</th>'
+        htmlcode += '                <th>Trift zu</th>'
+        htmlcode += '                <th>Gewichtung</th>'
+        htmlcode += '              </tr>'
+        for row in cur:
+            print row
+            htmlcode += '          <tr>'
+            htmlcode += '            <td>' + row[1] + '</td>'
+            htmlcode += u'            <td>' + unicode(row[2]) + '</td>'
+            htmlcode += '            <td>Trift zu:<input type="checkbox" name="' + str(row[0]) \
+                + '" value="' + str(row[0]) + '" /></td>'
+            htmlcode += '            <td><select name="' + str(row[0]) + '-wichtung" size="1">'
+            htmlcode += '                  <option value="1">mittel</option>'
+            htmlcode += '                  <option value="2">sehr wichtig</option>'
+            htmlcode += '                  <option value="0">unwichtig</option>'
+            htmlcode += '                  <option>Nina Hagen</option>'
+            htmlcode += '                </select>'
+            htmlcode += '            </td>'
+            htmlcode += '          </tr>'  
+        htmlcode += '           </table>'
+        htmlcode += form.Button('go').render()
+        htmlcode += '            </form>'
+        htmlcode += htemp.bottom
+        return htmlcode  
         
+    def POST(self): 
+    
+        #_form = test_form() 
+
+        #hiltirepo = _form['source'].value
+        #yes_or_no = _form['yes_or_no'].value
+        #print "value:" + yes_or_no
+        #searchTerms = web.input()
+        
+        htmlcode = htemp.top("datenbasis")
+        htmlcode += '        <h2>Datenbasis</h2>'
+        
+        widgetlist = web.input(groups = []) 
+        print "widgetlist: " 
+        print  widgetlist
+        for i in range(1, 6):  
+            if str(i) in widgetlist: 
+                htmlcode += 'YES' + str(i) + '<br>'
+            else:
+                htmlcode += 'NO' + str(i) + '<br>'
+            htmlcode += 'WICHTUNG: ' + widgetlist[ str(i) + '-wichtung'] + '<br>'
+                
+            #str(i) + "-wichtung"
+                #-wichtung
+                #htmlcode += 'YES' + str(i) + '<br>'
+            #else:
+                #htmlcode += 'NO' + str(i) + '<br>'
+                
+        htmlcode += htemp.bottom
+        return htmlcode           
 
 if __name__ == "__main__":
     app.run()
